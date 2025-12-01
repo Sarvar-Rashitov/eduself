e737aaeb3cee87d2d35355d7ce6cd92edf49d5bf
Bump Audrey <bumpaudr@te-a7.tash.21-school.uz>
2025-12-02 00:45:13 +0500

    ya

--- eduself/settings.py
+++ eduself/settings.py
@@ -90,10 +90,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT =  [BASE_DIR / 'staticfiles']
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = [BASE_DIR / 'media']
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
