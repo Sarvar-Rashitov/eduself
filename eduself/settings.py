b55a91aec30517ce217de1bb4c21f3a81390be3e
Bump Audrey <bumpaudr@te-a7.tash.21-school.uz>
2025-12-02 00:38:51 +0500

    Tayyor

--- eduself/settings.py
+++ eduself/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SESSION_SECRET', 'django-insecure-dev-key-change-in-production')
 
-DEBUG =  not os.environ.get("RENDER")
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
