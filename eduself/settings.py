98fe98652063b252305c6fe83b12d432c83b18ea
Bump Audrey <bumpaudr@sh-c9.tash.21-school.uz>
2025-12-07 08:24:00 +0500

    deploy

--- eduself/settings.py
+++ eduself/settings.py
@@ -94,7 +94,7 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = "/opt/render/project/src/media"
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
