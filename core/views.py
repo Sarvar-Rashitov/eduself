29e36ddd3fe4cc7203ef24e394a5a6c411ff2ad9
bumpaudr <sarvarrashitov4321@gmail.com>
2026-02-20 00:29:13 +0500

    Yangicha boldi

--- core/views.py
+++ core/views.py
@@ -35,7 +35,7 @@ def custom_404_view(request, exception=None):
 def home_view(request):
     subjects = Subject.objects.filter(is_active=True)[:3]  # 3 ta fan ko'rsatish
     certificates = Certificate.objects.filter(is_active=True)[:3]
-    institutions = Institution.objects.filter(is_featured=True, is_active=True)[:3]
+    institutions = Institution.objects.filter(is_featured=True, is_active=True)[:4]
     advertisements = Advertisement.objects.filter(is_active=True)[:5]
     partners = Partner.objects.filter(is_active=True).order_by('order')  # Hamkorlar
     
