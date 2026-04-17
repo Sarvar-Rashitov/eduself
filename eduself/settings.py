235a54e29cf02129b943ad8ec3e3b4aaa9d8db37
bumpaudr <sarvarrashitov4321@gmail.com>
2026-04-18 01:47:45 +0500

    Dump database

--- eduself/settings.py
+++ eduself/settings.py
@@ -194,9 +194,19 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'eduself.wsgi.application'
 
-DATABASES = {
-    'default': dj_database_url.config(default=env("DATABASE_URL"))
-}
+# Database configuration
+# SQLite yoki PostgreSQL (environment variable orqali)
+if os.environ.get('USE_SQLITE') == 'True':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+else:
+    DATABASES = {
+        'default': dj_database_url.config(default=env("DATABASE_URL"))
+    }
 
 
 
